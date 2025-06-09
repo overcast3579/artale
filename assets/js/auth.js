279cbf41e55f7016621829cf51bd3ca03b355c9d
Chris Huang <overcast3579@gmail.com>
2025-06-09 10:44:23 +0800

    --amend

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,8 +1,6 @@
-function checkLogin({
-    navbarPath = "../common/navbar.html",
-    redirectPath = "../index.html",
-    basePath = "../"
-} = {}) {
+function checkLogin() {
+    const navbarPath = "./common/navbar.html";
+    const redirectPath = "../index.html";
     const navbarContainer = document.getElementById("navbar-container");
     if (!navbarContainer) return;
 
@@ -10,12 +8,10 @@ function checkLogin({
         .then(res => res.text())
         .then(html => {
             navbarContainer.innerHTML = html;
-            console.log('navbarContainer', navbarContainer)
         })
         .then(() => {
             const isLoggedIn = localStorage.getItem("loggedIn") === "1";
             const account = localStorage.getItem("account");
-console.log('account', account)
             if (!isLoggedIn) {
                 location.href = redirectPath;
                 return;
